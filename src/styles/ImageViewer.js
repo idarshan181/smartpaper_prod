c2a5981676aed1b077451b9953fdb220375beb97
Darshan Patel <ddpatel97@gmail.com>
2022-01-13 00:46:26 +0530

    Fixed: Image size inside paper/box and made it responsive with next/image component

--- src/styles/ImageViewer.js
+++ src/styles/ImageViewer.js
@@ -1,25 +1,33 @@
 import styled from "styled-components";
 
 const ImageViewer = styled.div`
-  margin: 10px auto;
-  .output-label {
-    font-size: 1.2rem;
+  display: block;
+  margin-top:10px;
+  position: relative;
+  width: 100%;
+  .outputLabel {
+    font-size: 1rem;
     text-align: center;
+    font-weight: bold;
+    font-family: "Open Sans";
+    margin-top: 8px;
+    margin-bottom: 8px;
   }
-  .output-image {
+  .outputImage {
     width: 300px;
+    height: auto;
     max-width: 350px;
-    margin: 10px 5px 20px;
+    margin: 8px 5px 12px;
     border-radius: 8px;
     box-sizing: border-box;
     filter: drop-shadow(9px 9px 9px rgba(0, 0, 0, 0.3));
   }
   @media (max-width: 400px) {
     margin: auto;
-    .output-image {
-      width: 100%;
+    .outputImage {
+      width: 300px;
       max-width: 100%;
-      margin: 10px 5px 20px;
+      margin: 10px 5px 12px;
       border-radius: 8px;
       box-sizing: border-box;
       filter: drop-shadow(9px 9px 9px rgba(0, 0, 0, 0.3));
