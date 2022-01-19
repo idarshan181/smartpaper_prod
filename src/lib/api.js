503bf062cf2fbc7f285204c6e364ce61e680d022
Darshan Patel <ddpatel97@gmail.com>
2022-01-19 22:22:38 +0530

    Added: Test Fetch, Metadata fetch, result fetch apis in one place

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,30 +1,47 @@
-import axios from 'axios'
-const FormData = require('form-data')
+import axios from 'axios';
+const FormData = require('form-data');
 
 const encodeImageFileAsURL = async file =>
   new Promise(resolve => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onloadend = function () {
-      const base64data = reader.result
-      resolve(base64data)
-    }
-  })
+      const base64data = reader.result;
+      resolve(base64data);
+    };
+  });
 
 const getScanResult = async (testName, testImages, orgName) => {
-  const url = `https://api.smartpaperapp.com/api/smartpaper/scanAssessment-form`
-  const formData = new FormData()
+  const url = `https://api.smartpaperapp.com/api/smartpaper/scanAssessment-form`;
+  const formData = new FormData();
 
-  formData.append('testName', testName)
-  formData.append('orgName', orgName)
-  formData.append('testImages', testImages[0])
+  formData.append('testName', testName);
+  formData.append('orgName', orgName);
+  formData.append('testImages', testImages[0]);
 
   const result = await axios.post(url, formData, {
     headers: {
       ...formData.getHeaders
     }
-  })
-  return result
-}
+  });
+  return result;
+};
 
-export { encodeImageFileAsURL, getScanResult }
+const getPageMetadata = async pageIds => {
+  const result = await axios.post(
+    'https://prod.paperflowapp.com/authoring-page-metadata/pagemetadata/getPageMetadataDetails',
+    {
+      pageIds
+    }
+  );
+  return result;
+};
+
+const fetchAllTests = async () => {
+  const result = await axios.get(
+    'https://api.smartpaperapp.com/api/smartpaper/allAssessments'
+  );
+  return result;
+};
+
+export { encodeImageFileAsURL, getScanResult, getPageMetadata, fetchAllTests };
