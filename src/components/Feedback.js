b1b17777abe6bf0e8bd032e2240e9e06aeb7be8b
Darshan Patel <ddpatel97@gmail.com>
2022-02-08 21:37:00 +0530

    Created: Custom canvas component to provide clickable feedback

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -5,47 +5,71 @@ import { createRef, useEffect, useState } from 'react';
 import { Image, Layer, Stage } from 'react-konva';
 import useImage from 'use-image';
 import Konva from 'konva';
+import CustomDialog from './CustomDialog';
 function Feedback({ url, width, height, metadata }) {
   const [image] = useImage(url, 'Anonymous');
   const resultImage = createRef();
+  const stageRef = createRef();
+
+  const [open, setOpen] = useState(false);
+  const [ans, setAns] = useState();
+  const handleClickOpen = item => {
+    setAns(item);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    // setTimeout(() => {
+    //   setOpen(false);
+    // }, 1500);
+    setOpen(false);
+  };
+  const reportError = () => {
+    if (
+      window.confirm('Are you sure you want to report this answer for error?')
+    ) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
   const [state, setState] = useState({
-    heightRatio: 0,
-    widthRatio: 0,
-    ansBox: [],
-    ans: []
+    heightRatio: 0.56,
+    widthRatio: 0.52
   });
   //Note: Use props instead of state to avoid re-rendering.
   useEffect(() => {
     if (image) {
       resultImage.current.cache();
-      // console.log(url);
     }
+
     let answers = [];
     let ansBox = [];
-    if (metadata.length !== 0) {
-      metadata.data.map((item, index) => {
-        answers.push(item.ans);
-        ansBox.push(item.ansBox);
-      });
-      drawAnswers(answers, ansBox);
-    }
-    /* if (metadata.length > 0) {
-      // drawLabels();
-      
-    } */
     const { height: imageHeight, width: imageWidth } =
       resultImage.current.attrs;
-    console.log(imageHeight, imageWidth);
     const heightRatio = imageHeight / 1280;
     const widthRatio = imageWidth / 960;
 
-    setState(prevState => ({
-      ...prevState,
+    setState({
       heightRatio: heightRatio,
-      widthRatio: widthRatio,
-      ansBox: ansBox.flat(),
-      ans: answers.flat()
-    }));
+      widthRatio: widthRatio
+    });
+    if (metadata.length !== 0) {
+      //metadata.data.map
+      console.log(metadata);
+      metadata.map((item, index) => {
+        let res = {};
+        res['studentAnswer'] = item.studentAnswer;
+        res['correctAnswer'] = item.correctAnswer;
+        res['color'] = item.color;
+        res['coOrds'] = item.coOrds;
+        res['queNo'] = item.queNo;
+        res['cropsURL'] = item.cropsURL;
+        answers.push(res);
+      });
+      drawAnswers(answers);
+    }
     return null;
   }, [url, metadata]);
 
@@ -53,39 +77,47 @@ function Feedback({ url, width, height, metadata }) {
     resultImage.current.cache();
   };
 
-  const drawAnswers = (answers, ansCrops) => {
+  const drawAnswers = async answers => {
     const stage = resultImage.current.getStage();
     const layer = resultImage.current.getLayer();
     const image = resultImage.current.getImage();
-    console.log(layer);
-    /* const { width: imageWidth, height: imageHeight } = image.attrs;
-    const { width: stageWidth, height: stageHeight } = stage.attrs;
-    const { width: layerWidth, height: layerHeight } = layer.attrs; */
-    const answer = answers.flat();
-    const { widthRatio, heightRatio } = state;
-    ansCrops.flat().map((item, index) => {
-      const { w, h, x, y } = item;
+
+    const { height, width } = resultImage.current.attrs;
+    const heightRatio = height / 1280;
+    const widthRatio = width / 960;
+    answers.map((item, index) => {
+      let { x, y, h, w } = item.coOrds;
+      let [b, g, r] = item.color;
+      const { studentAnswer, correctAnswer } = item;
       const rect = new Konva.Rect({
-        x: Math.floor(x * widthRatio),
-        y: Math.floor(y * heightRatio) - 4,
+        x: Math.floor((x - 10) * widthRatio),
+        y: Math.ceil(y * heightRatio),
         width: Math.ceil(w * widthRatio),
         height: Math.ceil(h * heightRatio),
-        stroke: 'rgba(0,255,255,2)',
+        stroke: `rgba(${r},${g},${b},2)`,
         strokeWidth: 1,
         fill: '',
         opacity: 0.7,
         draggable: false
       });
+
       layer.add(rect);
       rect.on('mouseover', () => {
         stage.container().style.cursor = 'pointer';
       });
+      rect.on('mouseout', () => {
+        stage.container().style.cursor = 'default';
+      });
       rect.on('click', () => {
-        console.log(answer[index]);
-        console.log(stage);
+        handleClickOpen(item);
       });
     });
     stage.draw();
+    // await Promise.all(setRect).then(() => {
+    //   console.log('insidePromise');
+
+    // });
+
     /* const ansBoxRatio = ansBoxHeight / imageHeight;
     const ansRatio = ansHeight / imageHeight;
     const ansBoxWidthRatio = ansBoxWidth / imageWidth;
@@ -148,7 +180,8 @@ function Feedback({ url, width, height, metadata }) {
         height={height}
         style={{
           filter: 'drop-shadow(9px 9px 9px rgba(0, 0, 0, 0.3))',
-          borderRadius: '8px'
+          borderRadius: '8px',
+          paddingTop: '8px'
         }}
       >
         <Layer>
@@ -156,18 +189,26 @@ function Feedback({ url, width, height, metadata }) {
             ref={resultImage}
             image={image}
             alt="Result Image"
-            filters={[Konva.Filters.Brighten]}
             brightness={0.1}
             shadowBlur={8}
             width={width}
             height={height}
-            onClick={() => console.log(resultImage)}
+            style={{
+              marginTop: '2px'
+            }}
+            id="feedbackImage"
           />
         </Layer>
       </Stage>
-      {/* <Button variant="contained" onClick={viewCrops}>
-        Set Crops
-      </Button> */}
+      {ans && (
+        <CustomDialog
+          open={open}
+          onClose={handleClose}
+          title={'Student Answer'}
+          ans={ans}
+          reportError={reportError}
+        />
+      )}
     </div>
   );
 }
