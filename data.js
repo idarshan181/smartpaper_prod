152d1d483fd35214c33e5e63044c0626a8114054
Darshan Patel <ddpatel97@gmail.com>
2022-02-08 21:35:15 +0530

    Added: Sample Test data to fetch from

---
+++ data.js
@@ -0,0 +1,37 @@
+export const tests = [
+  {
+    id: '1',
+    name: 'Test 1',
+    school: 'School 1',
+    grade: '3-A',
+    subject: 'Maths'
+  },
+  {
+    id: '2',
+    name: 'Test 2',
+    school: 'School 2',
+    grade: '3-B',
+    subject: 'English'
+  },
+  {
+    id: '3',
+    name: 'Test 3',
+    school: 'School 1',
+    grade: '4-A',
+    subject: 'Gujarati'
+  },
+  {
+    id: '4',
+    name: 'Test 4',
+    school: 'School 1',
+    grade: '3-A',
+    subject: 'Maths'
+  },
+  {
+    id: '5',
+    name: 'Test 5',
+    school: 'School 1',
+    grade: '3-A',
+    subject: 'Maths'
+  }
+];
