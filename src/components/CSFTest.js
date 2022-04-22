ce1bf7ea389d6674471d52bae6880cae6899a0bc
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-04-22 12:52:22 +0530

    compressor types

--- src/components/CSFTest.js
+++ src/components/CSFTest.js
@@ -17,6 +17,7 @@ import { createRef, useState } from 'react';
 
 import { getScanResult } from '@/libs/api';
 import useForm from '@/libs/useForm';
+import Resizer from 'react-image-file-resizer';
 
 import { CSFTestNames } from '@/data/csf';
 
@@ -31,7 +32,25 @@ import ImageViewer from '@/styles/ImageViewer';
 
 import ErrorMessage from './ErrorMessage';
 import Loader from './Loader';
+import Compressor from 'compressorjs';
 
+const resizeFile = file =>
+  new Promise(resolve => {
+    Resizer.imageFileResizer(
+      file, //file name
+      1600, //max width
+      1600, //ht
+      'jpeg', //format
+      90, //quality
+      0, //rotation
+      uri => {
+        resolve(uri);
+      },
+      'file',
+      720,
+      1280      
+    );
+  });
 export default function CSFTest() {
   const [state, setState] = useState({
     orgName: 'CSF',
@@ -84,10 +103,31 @@ export default function CSFTest() {
       
       // Note:  Just to show it in the image component
       const fileList = Object.values(files);
+      // fileList.map((file, id) => {
+      //   console.log(`original -${id}`, file);
+      //   new Compressor(file, {
+      //     quality: 0.9,
+      //     success: (res)=> {
+      //       console.log(res);
+      //     }
+      //   })
+      // });
       //
       // compressor
       //(fileList)
       //
+      fileList.map((file, id) => {
+        console.log(`original-${id}`, file);
+        /* const out = resizeFile(file);
+        console.log(out);
+        final.push(out); */
+        resizeFile(file)
+          .then(res => {
+            console.log(`using image resizer-${id}`, res);
+            // final.push(res);
+          })
+          .catch(err => console.log(err));
+      });
       const source = await Promise.all(
         fileList.map(async file => URL.createObjectURL(file))
       );
--- src/components/CustomCompressor.js
+++ src/components/CustomCompressor.js
@@ -1,19 +1,49 @@
+// import fs from "fs";
+// import convert from "heic-convert";
+// import heic2any from "heic2any";
+// import { promisify } from "util";
 import Resizer from 'react-image-file-resizer';
+
 const resizeFile = file =>
   new Promise(resolve => {
     Resizer.imageFileResizer(
-      file,
-      3000,
-      3000,
-      'jpeg',
-      80,
-      0,
+      file, //file name
+      3000, //max width
+      3000, //ht
+      'png', //format
+      100, //quality
+      0, //rotation
+      
       uri => {
         resolve(uri);
       },
-      'file'
+
+      'file',
+      1440,
+      2560
     );
   });
+
+  // const handleHeic = async (e) => {
+  //   const {files} = e.target;
+  //   if (files.length === 0) return;
+  //   const fileList = Object.values(files);
+  
+  //   fileList.map((file, id) => {
+  //     console.log(`heic img - ${id}`, file);
+  //     console.log(file.stream());
+  //     // const buffer = await new Blob([file], { type: "image/heic"}).arrayBuffer();
+  //     // console.log(buffer.byteLength); 
+  //     // const img = await convert ({
+  //     //   buffer: file.stream(),
+  //     //   format: "JPEG",
+  //     //   quality: 1
+  //     // });
+  //     console.log(file);
+  //   })
+  // }  
+
+
 export default function CustomCompressor() {
   const handleFile = async e => {
     const { files } = e.target;
@@ -33,6 +63,36 @@ export default function CustomCompressor() {
         .catch(err => console.log(err));
     });
   };
+  // const handleHeic = async e => {
+  //   const { files } = e.target;
+  //   if (files.length === 0) return;
+  //   let final = [];
+  //   const fileList = Object.values(files);
+  //   fileList.map((file, id) => {
+  //     console.log(`original ${id}`, file);
+  //     const out = file.blob();
+  //     heic2any()
+  //   })
+  // }
+  // const handleHeic = async (e) => {
+  //   const {files} = e.target;
+  //   if (files.length === 0) return;
+  //   const fileList = Object.values(files);
+  
+  //   fileList.map((file, id) => {
+  //     console.log(`heic img - ${id}`, file);
+  //     console.log(file.stream());
+  //     // const buffer = await new Blob([file], { type: "image/heic"}).arrayBuffer();
+  //     // console.log(buffer.byteLength); 
+  //     // const img = await convert ({
+  //     //   buffer: file.stream(),
+  //     //   format: "JPEG",
+  //     //   quality: 1
+  //     // });
+  //     console.log(file);
+  //   })
+  // }  
+  
   return (
     <div>
       <input
