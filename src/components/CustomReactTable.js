5f7e11df630cba312201813db43530ed3f7e6df7
Darshan Patel <ddpatel97@gmail.com>
2022-04-27 00:34:04 +0530

    Removed: .vercel and unused imports

--- src/components/CustomReactTable.js
+++ src/components/CustomReactTable.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { useGlobalFilter,  useTable, useSortBy } from 'react-table'
+/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+import React from 'react';
+import { useGlobalFilter, useSortBy, useTable } from 'react-table';
 
 export default function CustomReactTable({ columns, data }) {
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-        state,
-        setGlobalFilter,
-    } = useTable({
-        columns,
-        data
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state
+  } = useTable(
+    {
+      columns,
+      data
     },
     useGlobalFilter,
-    useSortBy)
+    useSortBy
+  );
 
-    const { globalFilter } = state;
+  const { globalFilter } = state;
 
   return (
     <table {...getTableProps}>
@@ -27,26 +31,24 @@ export default function CustomReactTable({ columns, data }) {
             {headerGroup.headers.map(column => (
               <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                 {column.render('Header')}
-                {column.isSorted ? (column.isSortedDesc ? "↓":"↑"): ""}
-                </th>
+                {column.isSorted ? (column.isSortedDesc ? '↓' : '↑') : ''}
+              </th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
         {rows.map((row, i) => {
-          prepareRow(row)
+          prepareRow(row);
           return (
-            <tr {...row.getRowProps()} >
+            <tr {...row.getRowProps()}>
               {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>
-                  {cell.render('Cell')}
-                  </td>
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
+  );
 }
