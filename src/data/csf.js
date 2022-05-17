c76348549ef6754db92e78bc98ec2114c2396496
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-05-17 13:04:06 +0530

    added demo mcq 3

--- src/data/csf.js
+++ src/data/csf.js
@@ -1,4 +1,4 @@
 // export const CSFSchools = ['School 1', 'School 2', 'School 3'];
 // export const CSFGrades = ['3-A', '3-B', '4-A', '4-B', '8-A', '8-B'];
 // export const CSFSubjects = ['Maths', 'Gujarati', 'English'];
-export const CSFTestNames = ['Demo MCQ 1', 'Demo MCQ 2'];
+export const CSFTestNames = ['Demo MCQ 1', 'Demo MCQ 2', 'test_page4_1', 'Demo MCQ 3'];
