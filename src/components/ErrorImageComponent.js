fa41596fa2b0e31758243b62dc402b10118fad55
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-05-17 13:04:52 +0530

    feat: error image with border and close option

--- src/components/ErrorImageComponent.js
+++ src/components/ErrorImageComponent.js
@@ -1,33 +1,43 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const ErrorImageComponent = props => {
-  const { requestArray, errorProps } = props;
-
+  const { requestArray, errorProps, deleteErrorImg } = props;
+  // console.log("req array", requestArray);
   const newFoo = errorProps.map((element, id) => {
-    // errImages.push(
     let errImg = requestArray.find(
       errImgElement => errImgElement.requestId === element.errorId
     );
-    console.log('err', errImg.imageSource);
+
     return (
-      <div key={id}>
+      <div className="error image" key={id} style={{ border: '1px solid red' }}>
+        
+        <div style={{ display: 'flex', height: '20px', textAlign:'center', justifyContent:'space-between', alignItems:'center' }}>
+          <span> Error</span>
+          <button
+            onClick={() => {
+              deleteErrorImg(errImg)
+            }}
+            style={{ borderRadius: '50px',  border:'1px solid white',cursor:"pointer" }}
+          >
+            x
+          </button>
+        </div>
         <Image
           src={errImg.imageSource}
           key={id}
           alt={`error image ${id}`}
           className="outputImage"
           width={2}
-          height={3}
+          height={1}
           position="relative"
           layout="responsive"
           objectFit="contain"
         />
+        {/* </Alert> */}
+        {/* </Box> */}
       </div>
     );
-
-    // );
   });
-  // console.log("error",errImages);
   return <div>{newFoo}</div>;
 };
 
